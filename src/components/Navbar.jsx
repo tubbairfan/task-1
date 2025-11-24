dcf23e10b72b480fa77ba0f0cb83f629c0d680b5
tubbairfan <tubbairfan14@gmail.com>
2025-11-24 22:02:18 +0500

    resp2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,58 +1,42 @@
 import { useState } from "react";
 const Navbar = () => {
-  const items = [
-    "Jadoo",
-    "Destination",
-    "Hostels",
-    "Flights",
-    "Booking",
-    "Login",
-    "Signup"
-  ];
+  const items = ["Destination", "Hostels", "Flights", "Booking"];
   const itItems = ["ES", "FR", "DE", "IT"];
   const [selectedIt, setSelectedIt] = useState("IT");
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleSelect = (item) => {
+  // const [showDropdown, setShowDropdown] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMobileSelect = (item) => {
     setSelectedIt(item);
-    setShowDropdown(false);
+    // setShowDropdown(false); 
   };
 
-   return (
+  return (
     <nav className="navbar">
-      <ul className="nav-list">
-        {items.map((item, index) => (
-          <li
-            key={item}
-            className={index === 0 ? "nav-logo" : "nav-item"}
-          >
+      <h2 className="nav-logo">Jadoo</h2>
+      <div className="hamburger" onClick={() => setMenuOpen(true)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`nav-list ${menuOpen ? "mobile-open" : ""}`}>
+        <li className="mobile-close" onClick={() => setMenuOpen(false)}>
+          &times;
+        </li>
+
+        {items.map((item) => (
+          <li key={item} className="nav-item" onClick={() => setMenuOpen(false)}>
             {item}
           </li>
         ))}
         
-        <li
-          className="nav-item dropdown dropdown-button"
-          onMouseEnter={() => setShowDropdown(true)}
-          onMouseLeave={() => setShowDropdown(false)}
-        >
-          {selectedIt} <span className="dropdown-icon">▼</span>
-          {showDropdown && (
-            <ul className="dropdown-menu">
-              {itItems.map((it) => (
-                <li
-                  key={it}
-                  className="dropdown-item"
-                  onClick={() => handleSelect(it)}
-                >
-                  {it}
-                </li>
-              ))}
-            </ul>
-          )}
+        <li className="nav-item login-btn" onClick={() => setMenuOpen(false)}>
+          Login
+        </li>
+        <li className="nav-item signup-btn" onClick={() => setMenuOpen(false)}>
+          Signup
         </li>
       </ul>
     </nav>
   );
 };
-
 export default Navbar;
